abf7ac7f633bee2d39ce07f3b49c3c0a2d210d34
SharpGuanaco <siruvuri.kaushik@gmail.com>
2022-11-07 15:36:46 -0500

    comments and more bug fixes

--- ControllerSketch/ControllerSketch.ino
+++ ControllerSketch/ControllerSketch.ino
@@ -1,6 +1,8 @@
+//2023 FIASCO Finger Controller Sketch
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+//finger detection pins
 int thumbDetector = 4;
 int pointerDetector = 3;
 int middleDetector = 2;
@@ -9,7 +11,10 @@ int pinkyDetector = 6;
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
 //Thumb, Pointer, Middle, Ring, Pinky
+//To add commands, increment the number below to the corresponding amount
 const int numMessages = 6;
+//next add the wanted trigger to the end of the array, in {thumb,pointer,middle,ring,pinky} format. 
+//True means the finger is contracted while false means the finger is extended
 const bool checks[numMessages][5] = {
   {true,true,true,true,true},
   {true,true,false,true,true},
@@ -18,7 +23,9 @@ const bool checks[numMessages][5] = {
   {true,false,false,true,true},
   {true,false,false,false,false}
 };
-// related Strings
+//related Strings, add the wanted command to the end of the list
+//not actually transmitted, but useful for troubleshooting. The other arduino converts the index of the list to the necessary message.
+// Make sure to copy over to other sketch
 const String messages[numMessages] = {
   "GRAB",
   "FORWARD",
@@ -46,8 +53,11 @@ void setup() {
 }
 
 void loop() {
+  //check for changes in finger positions
   const int messageIndex = checkTriggered();
+  //send updates to robot controller arduino
   radio.write(&messageIndex, sizeof(int));
+  //delay to allow for finger movements, adjust as needed
   delay(1000);
 }
 
@@ -55,7 +65,9 @@ void loop() {
 int checkTriggered(){
   String message = "None";
   int messageIndex = -1;
+  //create array of all finger positions
   bool triggered[5] = {digitalRead(thumbDetector),digitalRead(pointerDetector),digitalRead(middleDetector),digitalRead(ringDetector),digitalRead(pinkyDetector)};
+  //check to see if array matches any known configurations
   for(int i = 0; i < numMessages; i++){
     bool check = true;
     for (int j = 0; j<5;j++){
@@ -68,6 +80,7 @@ int checkTriggered(){
         messageIndex = i;
       }
   }
+  //print out the message to be sent, and return index to be written to radio
   Serial.println(message);
   return messageIndex;
 }
--- RobotControllerSketch/RobotControllerSketch.ino
+++ RobotControllerSketch/RobotControllerSketch.ino
@@ -1,31 +1,54 @@
+//2023 FIASCO Robot Controller Sketch
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
 RF24 radio(7, 8); // CE, CSN
+//ESC controller pins
 int leftESC = 3;
 int rightESC = 4;
 
 const byte address[6] = "00001";
 
+//list of commands, to help translate recieved indexes
+//update both length and values as necessary
+const int messageLength = 6;
+const String messages[messageLength] = {
+  "GRAB",
+  "FORWARD",
+  "LEFT",
+  "RIGHT",
+  "BACK",
+  "TEST"
+};
+
 void setup() {
+  //initialize serial
   Serial.begin(9600);
+  //radio setup
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
+  //initialize output pins
   pinMode(leftESC, OUTPUT);
   pinMode(rightESC, OUTPUT);
 }
 
 void loop() {
+  //read radio to see if messages are sent
   if (radio.available()) {
     char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+    int received = -2;
+    radio.read(&received, sizeof(int));
+    if (received >= 0){
+      Serial.println(messages[received]);
+    }else{
+      Serial.println(received);
+    }
   }
 
-
+  //Serial input commands
   if (Serial.available()) {
     char c = toupper(Serial.read());
     if (c == 'C') {
