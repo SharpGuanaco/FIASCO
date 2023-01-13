8956ed173337a086e794d4ec2ed7ddd14c6dd5bf
SharpGuanaco <siruvuri.kaushik@gmail.com>
2023-01-13 13:19:15 -0500

    modular code + methods used for robot control

--- RobotCode/RobotCode.ino
+++ RobotCode/RobotCode.ino
@@ -10,21 +10,28 @@
 // radio setup
 RF24 radio(7, 8); // CE, CSN
 const byte address[6] = "00001";
+const int radioChannel = 1;
 
 //Robot Control Objects + Pins
 Servo left;
 Servo right;
 
-// Servo direction control constants
-// const int left = 70;
-// const int center = 97;
-// const int right = 110;
+const int leftServoPin = 2;
+const int rightServoPin = 3;
+const int ledPin = 5;
+
+//Servo values
 const int rightStopValue = 95;
 const int leftStopValue = 94;
 const int leftForwardVal = 0;
 const int rightForwardVal = 180;
 const int leftBackVal = 180;
 const int rightBackVal = 0;
+
+//Serial Commands
+const int SerialBaudRate = 9600;
+
+
 
 //list of commands, to help translate recieved indexes
 //update both length and values as necessary
@@ -41,25 +48,24 @@ const String messages[messageLength] = {
 
 void setup() {
   //initialize serial
-  Serial.begin(9600);
+  Serial.begin(SerialBaudRate);
   //radio setup
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.setAutoAck(false);
   radio.setDataRate(RF24_250KBPS);
-  radio.setChannel(1);
+  radio.setChannel(radioChannel);
   radio.startListening();
   radio.printDetails();
 
   //initialize robot controller pins
-  left.attach(2); 
-  right.attach(3);
-  //set initial direction to center and intial speed to 0
-  left.write(0);
-  right.write(0);
-
-  pinMode(5,OUTPUT);
+  left.attach(leftServoPin); 
+  right.attach(rightServoPin);
+  //set initial direction to center and initial speed to 0
+  stop();
+  //led setup
+  pinMode(ledPin,OUTPUT);
 }
 
 
@@ -69,23 +75,20 @@ void loop() {
     unsigned long received = 1000;
     radio.read(&received, sizeof(unsigned long));
     if (received >= 0 && received <= 999){
-      digitalWrite(5,LOW);
+      //change indicator led state
+      digitalWrite(ledPin,LOW);
       // move robot if commands are sent
       // turning direction commands are triggers, and stay in place until a new servo direction is set. 
       // Forward and reverse directions are only activated if continually recieved.
       String command = messages[received];
       if(command == "FORWARD"){ // forward
-        left.write(leftForwardVal);
-        right.write(rightForwardVal);
+        forward();
       }else if (command == "LEFT"){ // turning direction -> left
-        left.write(leftForwardVal);
-        right.write(rightBackVal);
+        turnLeft();
       }else if (command == "RIGHT"){ // turning direction -> right
-        left.write(leftBackVal);
-        right.write(rightForwardVal);
+        turnRight();
       }else if (command == "BACK"){ // reverse
-        left.write(leftBackVal);
-        right.write(rightBackVal);
+        back();
       }
       // print recieved message/command
       Serial.println(command);
@@ -93,9 +96,8 @@ void loop() {
       // blank command received
       Serial.println("NONE");
       // set speed to 0 when no forward or reverse command is received
-      left.write(leftStopValue);
-      right.write(rightStopValue);
-      digitalWrite(5,HIGH);
+      stop();
+      digitalWrite(ledPin,HIGH);
     }else{
       // error in receiving
       Serial.println("ERROR");
@@ -109,6 +111,42 @@ void loop() {
     // Clear Serial Monitor
     if (c == 'C') {
       Serial.println(F("Clearing\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"));
+    }else if (c == 'F') {
+      forward();
+    }else if (c == 'B') {
+      back();
+    }else if (c == 'L') {
+      turnLeft();
+    }else if (c == 'R') {
+      turnRight();
+    }else if (c == 'S') {
+      stop();
     }
   }
+
+}
+
+void turnLeft(){
+  left.write(leftForwardVal);
+  right.write(rightBackVal);
+}
+
+void turnRight(){
+  left.write(leftBackVal);
+  right.write(rightForwardVal);
+}
+
+void back(){
+  left.write(leftBackVal);
+  right.write(rightBackVal);
+}
+
+void forward(){
+  left.write(leftForwardVal);
+  right.write(rightForwardVal);
+}
+
+void stop(){
+  left.write(leftStopValue);
+  right.write(rightStopValue);
 }
